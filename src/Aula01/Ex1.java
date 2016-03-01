f8d14c8516ab596d42dcd5762662c077522bd1e0
ramonsl <ramonsl@gmail.com>
2016-03-01 16:47:03 -0300

    Initial commit

---
+++ src/Aula01/Ex1.java
@@ -0,0 +1,14 @@
+package Aula01;
+
+/**
+ * Created by ramon on 01/03/16.
+ */
+public class Ex1 {
+
+    public static void main(String args[]){
+
+        int mat[][] =new int [3][3];
+
+        System.out.println("Bia");
+    }
+}
