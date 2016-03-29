d97a25bf70dc6aea047d6eee21aaa668f7468984
ramonsl <ramonsl@gmail.com>
2016-03-29 20:33:52 -0300

    Pilha

---
+++ src/Aula06/Fila.java
@@ -0,0 +1,48 @@
+package Aula06;
+
+/**
+ * Created by ramon on 29/03/16.
+ */
+public class Fila {
+    int inicio;
+    int fim;
+    boolean isInit;
+    int[] vet = new int[10];
+
+    public boolean inicializar(){
+        this.inicio =-1;
+        this.fim=-1;
+        isInit=true;
+        return true;
+    }
+    public boolean isFull(){
+        if (this.fim==9){
+            System.out.println("Fila cheia");
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean add(int a){
+
+        if (isFull()){
+            System.out.println("Fila Cheia Windows Me");
+            return false;
+        }else{
+            if (this.isInit==true){
+                this.fim++;
+                this.vet[this.fim] =a;
+                System.out.println(this.vet[this.fim] +" adicionado");
+                return true;
+            }else{
+                System.out.println("Fila não inicializada");
+                return false;
+            }
+        }
+    }
+
+    public boolean remover(){
+        return true;
+    }
+}
